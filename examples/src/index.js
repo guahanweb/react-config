4116984e7596ec7acfdcc6caedfc8436db6e40f0
Garth Henson <garth.a.henson@disney.com>
2018-07-27 12:36:17 -0400

    Initial commit

---
+++ examples/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div className="app-wrapper">
+    <h1>Hello</h1>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, 'examples/src/index.html'),
+  filename: './index.html'
+});
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'examples/src/index.js')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: ['.js', 'jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000
+  }
+};
